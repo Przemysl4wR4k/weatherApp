dc7e6718eae9ec205f8c2286527698096db9ef12
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-22 16:38:39 +0100

    first successful call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  
+  private API_KEY = 'baeb4b74ccba157a14abf14eadaf3eb3' //normally this should've been stored in some other file, one that will not be pushed to git repo
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather'
+
+  constructor (private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    const params = {
+      q: 'Warsaw',
+      appid: this.API_KEY,
+      units: 'metric', 
+    }
+    this.httpClient.get(this.apiUrl, { params }).pipe(tap(response => console.log(response))).subscribe()
+  }
+  
+  title = 'weather-app'
 }
