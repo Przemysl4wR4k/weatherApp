8d20a6a504dbae7f6ad331b19bab2c2162b42880
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-22 17:20:47 +0100

    random cities interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { tap } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { startWith, tap } from 'rxjs/operators';
+
+
+const API_KEY = 'baeb4b74ccba157a14abf14eadaf3eb3' //normally this should've been stored in some other file, one that will not be pushed to git repo
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+const CITIES = ['Lodz', 'Warsaw', 'Berlin', 'New York', 'London']
 
 @Component({
   selector: 'app-root',
@@ -11,21 +17,29 @@ import { tap } from 'rxjs/operators';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  
-  private API_KEY = 'baeb4b74ccba157a14abf14eadaf3eb3' //normally this should've been stored in some other file, one that will not be pushed to git repo
-  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather'
+
+  selectedCities: string[] = []  
 
   constructor (private httpClient: HttpClient) {
 
   }
 
   ngOnInit(): void {
+    interval(60000).pipe(
+      tap(() => this.selectRandomCities())
+    ).subscribe()
+
     const params = {
       q: 'Warsaw',
-      appid: this.API_KEY,
+      appid: API_KEY,
       units: 'metric', 
     }
-    this.httpClient.get(this.apiUrl, { params }).pipe(tap(response => console.log(response))).subscribe()
+    this.httpClient.get(API_URL, { params }).pipe(tap(response => console.log(response))).subscribe()
+  }
+  
+  selectRandomCities() {
+    this.selectedCities = CITIES.sort(() => Math.random() - 0.5).slice(0,3)
+    console.log(this.selectedCities)
   }
   
   title = 'weather-app'
