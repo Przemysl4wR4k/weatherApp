19b50ba8f18dbfb445a0ce05e0b0835d45a86c86
Przemysl4wR4k <przemyslawrak@interia.pl>
2024-03-22 18:03:27 +0100

    calls in intervals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { interval } from 'rxjs';
-import { startWith, tap } from 'rxjs/operators';
+import { Observable, Subject, forkJoin, interval, throwError } from 'rxjs';
+import { catchError, startWith, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 
 const API_KEY = 'baeb4b74ccba157a14abf14eadaf3eb3' //normally this should've been stored in some other file, one that will not be pushed to git repo
@@ -16,31 +16,52 @@ const CITIES = ['Lodz', 'Warsaw', 'Berlin', 'New York', 'London']
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements  OnDestroy {
 
-  selectedCities: string[] = []  
+  selectedCities: string[] = []
+  destroy$ = new Subject<void>()
 
   constructor (private httpClient: HttpClient) {
 
   }
 
-  ngOnInit(): void {
+  startFetching(): void {
     interval(60000).pipe(
-      tap(() => this.selectRandomCities())
+      startWith(0),
+      tap(() => this.selectCities()),
+      catchError((error: HttpErrorResponse) => throwError(() => error)),
+      takeUntil(this.destroy$)
     ).subscribe()
 
-    const params = {
-      q: 'Warsaw',
-      appid: API_KEY,
-      units: 'metric', 
-    }
-    this.httpClient.get(API_URL, { params }).pipe(tap(response => console.log(response))).subscribe()
+    interval(10000).pipe(
+      switchMap(() => this.getData()),
+      catchError((error: HttpErrorResponse) => throwError(() => error)),
+      takeUntil(this.destroy$)
+    ).subscribe()
   }
   
-  selectRandomCities() {
+  selectCities(): void {
     this.selectedCities = CITIES.sort(() => Math.random() - 0.5).slice(0,3)
     console.log(this.selectedCities)
   }
-  
+
+  getData(): Observable<any> {
+    return forkJoin(this.selectedCities.map(city => this.getWeatherByCity(city)))
+  }
+
+  getWeatherByCity(city: string): Observable<any> {
+    const params = {
+      q: city,
+      appid: API_KEY,
+      units: 'metric', 
+    }
+    return this.httpClient.get(API_URL, { params }).pipe(tap((response: any) => console.log(response.name)))
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next()
+      this.destroy$.complete()
+  }
+
   title = 'weather-app'
 }
